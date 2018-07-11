6952e799be9876fc6ccddcfcee006f25b3ecb933
PsyMan47 <xhonirexhepi@gmail.com>
2018-07-11 18:19:55 +0200

    Initial release

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,7 +1,4 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
-MODDIR=${0%/*}
 
-# This script will be executed in post-fs-data mode
-# More info in the main Magisk thread
+for i in $(find /sys/devices -name "enable_wakeup") ; do if grep -q 0 "$i" ; then echo 1 > $i ; fi ; done
+for i in $(find /sys/devices -name "wakeup_enable") ; do if grep -q 0 "$i" ; then echo 1 > $i ; fi ; done
--- config.sh
+++ config.sh
@@ -22,13 +22,13 @@
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
-AUTOMOUNT=true
+AUTOMOUNT=false
 
 # Set to true if you need to load system.prop
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
@@ -41,7 +41,11 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     Always-On Fingerprint     "
+  ui_print "*******************************"
+  ui_print "         FOR ANY DEVICE        "
+  ui_print "*******************************"
+  ui_print "    Psy_Man @ xda-developers   "
   ui_print "*******************************"
 }
 
